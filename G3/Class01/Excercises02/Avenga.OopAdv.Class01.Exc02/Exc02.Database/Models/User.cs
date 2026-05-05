6d9b0cc7768183245ae6d3baafe4f2f37def158e
Danilo Borozan <daniloborozan07@gmail.com>
2026-05-05 20:55:59 +0200

    add task 4

---
+++ G3/Class01/Excercises02/Avenga.OopAdv.Class01.Exc02/Exc02.Database/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exc02.Database.Models
+{
+    public class User
+    {
+        public string FullName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int CorrectAnswers { get; set; }
+        public bool HasFinishedTest { get; set; }
+        public bool IsTeacher { get; set; }
+    }
+}
---
+++ G3/Class01/Excercises02/Avenga.OopAdv.Class01.Exc02/Exc02.Logic/Services/UserService.cs
@@ -0,0 +1,55 @@
+﻿using Exc02.Database.Db;
+using Exc02.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exc02.Logic.Services
+{
+    public class UserService
+    {
+        private InMemoryDatabase _inMemoryDatabase;
+
+        public UserService()
+        {
+            _inMemoryDatabase = new InMemoryDatabase();
+        }
+
+        public User Login()
+        {
+            Console.Write("Enter username: ");
+            string username = Console.ReadLine();
+
+            var user = _inMemoryDatabase.GetUserByUserName(username);
+
+            if (user == null)
+            {
+                Console.WriteLine("Username does not exists.");
+                return null;
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Enter password: ");
+                string password = Console.ReadLine();
+
+                if (user.Password == password)
+                {
+                    return user;
+                }
+            }
+
+            Console.WriteLine("You missed password 3 times. Try again in 30 min");
+            Thread.Sleep(3000);
+            Environment.Exit(1);
+            return null;
+        }
+
+        public List<User> GetStudents()
+        {
+            return _inMemoryDatabase.GetStudents();
+        }
+    }
+}
