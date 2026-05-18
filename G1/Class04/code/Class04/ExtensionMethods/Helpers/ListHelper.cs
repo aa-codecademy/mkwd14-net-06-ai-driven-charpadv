20dab64e5f8e44a55b193aec10a36ab874f00fe4
ilijamitev <ilija.mitev3@gmail.com>
2026-05-18 19:24:35 +0200

    Class05

--- G1/Class04/code/Class04/ExtensionMethods/Helpers/ListHelper.cs
+++ G1/Class04/code/Class04/ExtensionMethods/Helpers/ListHelper.cs
@@ -2,12 +2,24 @@
 {
     public static class ListHelper
     {
+        /// <summary>
+        /// A Generic extension method that can be called on any list with items and print the list
+        /// </summary>
         public static void PrintItems<T>(this List<T> items)
         {
             foreach (T item in items)
             {
                 Console.WriteLine(item);
             }
+        }
+
+        /// <summary>
+        /// A Generic extension method that can be called on any list with items and print the list info
+        /// </summary>
+        public static void PrintListInfo<T>(this List<T> items) 
+        {
+            string listType = typeof(T).Name;
+            Console.WriteLine($"This list has {items.Count} elements and is of type {listType}.");
         }
     }
 }
--- G1/Class04/code/Class04/ExtensionMethods/Program.cs
+++ G1/Class04/code/Class04/ExtensionMethods/Program.cs
@@ -1,7 +1,14 @@
 ﻿using ExtensionMethods.Helpers;
+using ExtensionMethods.Models;
+using ExtensionMethods;
 
-Console.WriteLine("Hello, World!");
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("\n=================== EXTENSION METHODS ===================\n");
+Console.ResetColor();
 
+
+#region List Extensions examples
+Console.WriteLine("\n============== List Extensions ==============\n");
 List<int> integers = new() { 1, 2, 3, 4, 5 };
 List<string> strings = ["str1", "str2", "str3"];
 
@@ -9,6 +16,33 @@ List<string> strings = ["str1", "str2", "str3"];
 //ListHelper.PrintItems(strings);
 integers.PrintItems();
 strings.PrintItems();
+
+integers.PrintListInfo();
+#endregion
+
+
+#region String Extensions examples
+Console.WriteLine("\n============== String Extensions ==============\n");
+string bobLong = "Bob Bobsky";
+string bobShorten = bobLong.Truncate(3);
+Console.WriteLine(bobShorten);
+Console.WriteLine("Bob Bobsky".Truncate(5));
+
+string johnShort = StringExtensions.Truncate("John Doe", 1);
+Console.WriteLine(johnShort.Quote());
+#endregion
+
+
+#region Product Extensions & Piggybacking
+Console.WriteLine("\n============== Product Extensions ==============\n");
+Product product = new Product
+{
+    Id = 1,
+    Description = "Product Description",
+    Title = "Product Title"
+};
+product.PrintInfo();
+#endregion
 
 
 Console.ReadLine();
