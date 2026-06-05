2d136d8a44d1683401df7c0c4609795addfbfc9e
ilijamitev <ilija.mitev3@gmail.com>
2026-06-05 18:49:25 +0200

    Class 11

---
+++ G1/Class11/code/SerializationDeserialization/SerializationDeserialization.Demo/OurJsonSerializer.cs
@@ -0,0 +1,28 @@
+﻿namespace SerializationDeserialization.Demo
+{
+    internal static class OurJsonSerializer
+    {
+        public static string SerializeStudent(Student student)
+        { 
+
+
+            return string.Empty;
+        }
+
+        public static Student DeserializeStudent(string json)
+        {
+            // The JSON object:
+            /*
+                {
+                  "firstName": "Bob",
+                  "lastName": "Bobsky",
+                  "age": 33,
+                  "isPartTime": false
+                }
+            */
+
+
+            return null;
+        }
+    }
+}
---
+++ G1/Class11/code/SerializationDeserialization/SerializationDeserialization.Demo/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SerializationDeserialization.Demo;
+
+Console.WriteLine("Hello, World!");
+
+void WriteInFile(string path, string text)
+{
+	using (StreamWriter sw = new StreamWriter(path))
+	{
+		sw.WriteLine(text);
+	}
+}
+
+string ReadFromFile(string path)
+{
+    using (StreamReader sr = new StreamReader(path))
+    {
+        return sr.ReadToEnd();
+    }
+}
+
+string directoryPath = @"..\..\..\OurData";
+string filePath = Path.Combine(directoryPath, "myFirstJson.json");
+
+
+#region Manual Serialization/DeSerialization
+if (!Directory.Exists(directoryPath))
+{
+    Directory.CreateDirectory(directoryPath);
+}
+
+Student bob = new Student()
+{
+    FirstName = "Bob",
+    LastName = "Bobsky",
+    Age = 33,
+    IsPartTime = false
+};
+
+WriteInFile(filePath, bob.ToString());
+
+#endregion
+
+
+Console.ReadLine();
