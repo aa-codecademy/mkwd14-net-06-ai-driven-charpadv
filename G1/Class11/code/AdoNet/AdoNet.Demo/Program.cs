8693f772710c7f3d7a713f95196bf743ef532064
ilijamitev <ilija.mitev3@gmail.com>
2026-06-08 18:44:10 +0200

    Class 12

--- G1/Class11/code/AdoNet/AdoNet.Demo/Program.cs
+++ G1/Class11/code/AdoNet/AdoNet.Demo/Program.cs
@@ -18,7 +18,10 @@ void PrintStudents(List<Student> students)
     }
 }
 
-const string ConnectionString = "Server=.\\SQLEXPRESS;Database=SEDC_DEMO_SHARP;Trusted_Connection=True;Integrated Security=True;Encrypt=False;TrustServerCertificate=True;";
+// ===> For using SQL Express, the connection string should be like this:
+//const string ConnectionString = "Server=.\\SQLEXPRESS;Database=SEDC_DEMO_SHARP;Trusted_Connection=True;Integrated Security=True;Encrypt=False;TrustServerCertificate=True;";
+// ===> For using SQL Server, the connection string should be like this:
+const string ConnectionString = "Server=.;Database=SEDC_DEMO_SHARP;Trusted_Connection=True;Integrated Security=True;Encrypt=False;TrustServerCertificate=True;";
 
 StudentRepository studentRepository = new StudentRepository(ConnectionString);
 
@@ -29,6 +32,31 @@ PrintInColor("\n======= All Students =======", ConsoleColor.Cyan);
 PrintStudents(allStudents);
 
 
+PrintInColor("\n======= Insert New Student =======", ConsoleColor.Cyan);
+
+Console.WriteLine("\nEnter First Name:");
+// ===> SQL Injection Example
+// Insert the line bellow when calling InsertStudentSqlInjection
+// John', 'Doe', '2000-01-01', '2025-01-01', 'M', 1234567890123, 'CA-2024-001'); DROP TABLE Student;--
+
+string firstName = Console.ReadLine();
+
+if (!string.IsNullOrWhiteSpace(firstName))
+{
+    Student student = new Student
+    {
+        FirstName = firstName,
+        LastName = "Doe",
+        DateOfBirth = DateTime.UtcNow.AddYears(-32),
+        EnrolledDate = DateTime.UtcNow,
+        Gender = 'M',
+        NationalIdNumber = 23432454235,
+        StudentCardNumber = "ST-4234-bla"
+    };
+
+    //studentRepository.InsertStudentSqlInjection(student);
+    studentRepository.InsertStudentSafe(student);
+}
 
 
 
