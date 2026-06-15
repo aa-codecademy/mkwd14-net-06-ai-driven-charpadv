c9bea9bd6383c57663e67b858d1715dfa8593c24
Danilo Borozan <daniloborozan07@gmail.com>
2026-06-15 18:59:50 +0200

    add code

---
+++ G1/Class14/TaxiManager9000/TaxiManager9000.Services/Interfaces/IUIService.cs
@@ -0,0 +1,14 @@
+﻿using TaxiManager9000.Domain.Enums;
+using TaxiManager9000.Domain.Models;
+using TaxiManager9000.Services.Enums;
+
+namespace TaxiManager9000.Services.Interfaces
+{
+    public interface IUIService
+    {
+        int ChooseMenu<T>(List<T> items);
+        User LogInMenu();
+        int MainMenu(Role role);
+        List<MenuChoice> MenuItems { get; set;}
+    }
+}
