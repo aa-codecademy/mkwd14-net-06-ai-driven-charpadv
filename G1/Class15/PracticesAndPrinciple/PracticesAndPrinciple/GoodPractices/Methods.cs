3c5c07952607e4170634f6e1b2c169b3b21e80b2
Danilo Borozan <daniloborozan07@gmail.com>
2026-06-17 18:31:58 +0200

    add code

---
+++ G1/Class15/PracticesAndPrinciple/PracticesAndPrinciple/GoodPractices/Methods.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticesAndPrinciple.GoodPractices
+{
+    #region Bad Example
+    // Bad Example
+    internal class NumberServiceBad
+    {
+        public List<int> numbers = new List<int>();
+        public void GetStats()
+        {
+            Console.WriteLine("Welcome to the app!");
+            Console.WriteLine("Enter 5 numbers:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter number:");
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.Write("You entered: ");
+            foreach (int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Stats:");
+            int even = numbers.Where(x => x % 2 == 0).Count();
+            Console.WriteLine($"Even numbers: {even}");
+
+            int odd = numbers.Count - even;
+            Console.WriteLine($"Odd numbers: {odd}");
+
+            int positive = numbers.Where(x => x >= 0).Count();
+            Console.WriteLine($"Positive numbers: {positive}");
+
+            int negative = numbers.Count - positive;
+            Console.WriteLine($"Negative numbers: {negative}");
+
+            int sum = numbers.Sum();
+            Console.WriteLine($"Sum of numbers: {sum}");
+        }
+    }
+    #endregion
+
+    #region Good Example
+    internal class NumberService
+    {
+        public List<int> RequestNumbers(int numbersCount)
+        {
+            List<int> numbers = new List<int>();
+            Console.WriteLine("Welcome to the app!");
+            Console.WriteLine($"Enter {numbersCount} numbers:");
+            for (int i = 0; i < numbersCount; i++)
+            {
+                Console.Write("Enter number:");
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+            return numbers;
+        }
+        public void PrintNumbers(List<int> numbers)
+        {
+            Console.Write("You entered: ");
+            foreach (int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void PrintStats(List<int> numbers)
+        {
+            Console.WriteLine("Stats:");
+            int even = numbers.Where(x => x % 2 == 0).Count();
+            Console.WriteLine($"Even numbers: {even}");
+
+            int odd = numbers.Count - even;
+            Console.WriteLine($"Odd numbers: {odd}");
+
+            int positive = numbers.Where(x => x >= 0).Count();
+            Console.WriteLine($"Positive numbers: {positive}");
+
+            int negative = numbers.Count - positive;
+            Console.WriteLine($"Negative numbers: {negative}");
+
+            int sum = numbers.Sum();
+            Console.WriteLine($"Sum of numbers: {sum}");
+        }
+    }
+    #endregion
+}
---
+++ G1/Class15/PracticesAndPrinciple/PracticesAndPrinciple/GoodPractices/Naming.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticesAndPrinciple.GoodPractices
+{
+    #region CLASSES, PROPERTIES, FIELDS, METHODS, VARIABLES, NAMESPACES, PARAMETERS, ETC.
+    // BAD EXAMPLES:
+    internal class user
+    {
+        private readonly string usersFolder = @"C:\users";
+        public int id;
+        public string username;
+        public string Password;
+
+
+
+        public int age;
+        public bool Logged { get; set; } = false;
+        public List<string> HobbyList { get; set; }
+
+        public void Username(string Username)
+        {
+            this.username = Username;
+        }
+        public void changePwd(string pwd)
+        {
+            Password = pwd;
+        }
+        public async void GetUsers()
+        {
+            // code
+        }
+    }
+
+    internal class User
+    {
+        private readonly string _usersFolder = @"C:\users";
+
+        public int Age { get; set; }
+        public bool IsLoggedIn { get; set; }
+        public List<string> Hobbies { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User()
+        {
+            
+        }
+
+        public void ChangeUsername(string username)
+        {
+            Username = username;
+        }
+        public void ChangePassword(string password)
+        {
+            Password = password;
+        }
+        public async Task GetUsersAsync()
+        {
+            // code
+        }
+
+    }
+    #endregion
+
+    #region ENUMS
+    // BAD EXAMPLE:
+    public enum RoleEnum
+    {
+        Admin,
+        User,
+        Manager
+    }
+
+    // GOOD EXAMPLE:
+    public enum Role
+    {
+        Admin = 1,
+        User,
+        Manager
+    }
+    //public enum Role
+    //{
+    //    Admin = 1,
+    //    User = 2,
+    //    Manager = 3
+    //}
+    #endregion
+
+    #region INTERFACES
+    // BAD EXAMPLE:
+    public interface iuserrepository
+    {
+        //.... code
+    }
+    public interface UserService
+    {
+        //... code
+    }
+
+    // GOOD EXAMPLE:
+    public interface IUserRepository
+    {
+        //.... code
+    }
+    public interface IUserService
+    {
+        //... code
+    }
+    #endregion
+}
